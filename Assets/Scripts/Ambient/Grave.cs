845f2e70b665d5f2140b3e3fd1f0dfc5e3674b1b
Laura Rebelo <91252082+laurarebelo@users.noreply.github.com>
2024-12-05 03:23:22 +0100

    did graveyard UI:)
    and got started on some sorting/filtering functions but i'd need actual DeadFishGetObject to test them

--- Assets/Scripts/Ambient/Grave.cs
+++ Assets/Scripts/Ambient/Grave.cs
@@ -9,9 +9,12 @@ public class Grave : MonoBehaviour
     public TMP_Text fishNameText;
     public TMP_Text birthDateText;
     public TMP_Text deathDateText;
+    public TMP_Text respectCountText;
 
     public SpriteRenderer outlineSpriteRenderer;
     public SpriteRenderer colorSpriteRenderer;
+
+    public RectTransform respectParentRT;
 
     private FishTemplateProvider templateProvider;
 
@@ -29,10 +32,27 @@ public class Grave : MonoBehaviour
         fishNameText.text = deadFishGetObject.name;
         birthDateText.text = GetDateMiniString(deadFishGetObject.dateOfBirth);
         deathDateText.text = GetDateMiniString(deadFishGetObject.dateOfDeath);
+        respectCountText.text = deadFishGetObject.respectCount.ToString();
+        AdjustPosXDependingOnRespectCountDigits();
         outlineSpriteRenderer.sprite = templateProvider.GetSpritePair(deadFishGetObject.template).outlineSprite;
         colorSpriteRenderer.sprite = Utils.GetSpriteFromEncodedString(deadFishGetObject.sprite);
         colorSpriteRenderer.color = Color.white;
     }
+
+    private void AdjustPosXDependingOnRespectCountDigits()
+    {
+        float newX = respectCountText.text.Length switch
+        {
+            1 => 0f,
+            2 => -0.0925f,
+            3 => -0.319125f,
+            4 => -0.446775f,
+            _ => respectParentRT.localPosition.x
+        };
+
+        respectParentRT.localPosition = new Vector3(newX, respectParentRT.localPosition.y, respectParentRT.localPosition.z);
+    }
+
 
     public string GetDateMiniString(string dateStringYYYYMMDD)
     {
